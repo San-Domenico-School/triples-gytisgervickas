1f2263d9027e913ea550089dc3078c0f98e140f3
gytisgervickas <ggervickas23@sandomenico.org>
2023-01-19 14:01:31 -0800

    Jan 19
    5.3.2 finished

--- Animations.java
+++ Animations.java
@@ -1,10 +1,19 @@
+import greenfoot.*;
 /**
  * Write a description of class Animations here.
  * 
  * @Gytis Gervickas 
  * @January 17, 2023
  */
-public class Animations  
+public class Animations 
 {
-    
+    public static void wobble(Card[] card) 
+    {
+        Greenfoot.playSound("wobble.wav");
+    }
+
+    public static void slideAndTurn(Card[] card) 
+    {
+        Greenfoot.playSound("swoosh.wav");
+    }
 }
